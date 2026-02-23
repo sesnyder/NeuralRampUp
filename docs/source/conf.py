1b123bf3d7289988359e5ed1396c319b55c6cced
Sam <32720262+sesnyder@users.noreply.github.com>
2026-02-23 09:38:17 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,11 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "smile_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
